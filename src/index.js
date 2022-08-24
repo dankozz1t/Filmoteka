12b1a0d4e2db280bf6f6ff3d4f37465744f02f6f
Flameofwork <shvetsalexei@gmail.com>
2022-08-24 17:25:44 +0300

    add test-code for genres

--- src/index.js
+++ src/index.js
@@ -4,22 +4,52 @@ import { toggleBackdrop } from './js/backdrop.js';
 import { renderPagination } from './js/pagination.js';
 import templateRenderFilms from './templates/template-film.js';
 import templatePlugEmpty from './templates/template-plug-empty.hbs';
+import onSmoothScroll from './js/smoothScroll';
+
+onSmoothScroll();
 
 const apiService = new ApiService();
 
 refs.contentList.addEventListener('click', onGetInfoClick);
 refs.form.addEventListener('submit', onFormSubmit);
 
+//  data.results.map(film => {
+//    strGenres += genres
+//      .filter(({ id }) => film.genre_ids.includes(id))
+//      .map(({ name }) => name);
+//  });
+
 apiService.fetchFilms().then(({ data }) => {
+  if (!apiService.allGenres) {
+    apiService.fetchGenres();
+  }
+
+  // console.log(data.results);
+  let genreIDS = data.results.map(({ genre_ids }) => genre_ids);
+  genreIDS.forEach(gen => {
+    let strGenres = [];
+    apiService.allGenres.forEach(el => {
+      // console.log('EL ', el.id);
+      console.log('genreIDS ', gen);
+      if (gen.includes(el.id)) {
+        strGenres.push(el.name);
+      }
+    });
+
+    console.log(strGenres);
+  });
+
+  //let genreIDS = data.results.forEach(films => {
+  //let strGenres = [];
+  //films.genre_ids.forEach
+
+  // })
+
   renderFilms(data.results);
 
   renderPagination(apiService.page, apiService.totalPages);
 });
-//!
-apiService.fetchGenres().then(data => {
-  console.log(data);
-});
-//
+
 refs.paginationControls.addEventListener('click', onPaginationClick);
 refs.contentList.addEventListener('click', onGetInfoClick);
 
@@ -131,6 +161,6 @@ function renderFilms(arrayFilms) {
     return;
   }
 
-  console.log('RENDER - ', arrayFilms);
+  //console.log('RENDER - ', arrayFilms);
   refs.contentList.innerHTML = templateRenderFilms(arrayFilms);
 }
