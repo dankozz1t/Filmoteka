fd8e407fd46658eb45b7bccfb503e93248cc8a8a
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-08-25 08:47:37 +0300

    add plug in library

--- src/library.js
+++ src/library.js
@@ -1,5 +1,9 @@
+import { refs } from './js/references/reference.js';
 import { onTopArrow } from './js/components/lift-up.js';
 import { onSwitch } from './js//components/switch.js';
+import templatePlugEmpty from './templates/template-plug-empty.hbs';
 
 onTopArrow();
 onSwitch();
+
+refs.contentList.innerHTML = templatePlugEmpty();
