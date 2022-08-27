204a9fcd763ff23636fc63cc61fde82ebe1510ff
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-08-27 17:57:16 +0300

    move onGetInfoClick

---
+++ src/js/components/onGetInfoClick.js
@@ -0,0 +1,12 @@
+import { apiService } from '../API/api-service.js';
+import { fillModal } from './modal/fillModal.js';
+
+export function onGetInfoClick(e) {
+  const filmClick = e.target.parentNode.parentNode;
+  if (filmClick.nodeName !== 'LI') {
+    return;
+  }
+  const film = apiService.films.find(({ id }) => id == filmClick.id);
+
+  fillModal(film);
+}
