0aea83cf1aba562d9ce5f1115f2a73332ef15b11
DenisChum <105285389+DenisChum@users.noreply.github.com>
2022-08-28 19:05:01 +0200

    update spinner

--- src/js/components/spinner.js
+++ src/js/components/spinner.js
@@ -6,4 +6,10 @@ export function spinnerOn() {
 
 export function spinnerOff() {
   refs.spinner.classList.add('visually-hidden');
+  const one = document.querySelector('.inner one');
+  const two  =document.querySelector('.inner two');
+  const three = document.querySelector('.inner three');
+  one.classList.remove('inner one')
+  two.classList.remove('inner two')
+  three.classList.remove('inner three')
 }
