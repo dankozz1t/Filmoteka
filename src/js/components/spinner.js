d9f9143e8b8a28d350f3ac78af6bc19e15089414
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-08-28 22:40:44 +0300

    fix performance spinner

--- src/js/components/spinner.js
+++ src/js/components/spinner.js
@@ -1,9 +1,21 @@
 import { refs } from '../references/reference.js';
 
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+
 export function spinnerOn() {
   refs.spinner.classList.remove('visually-hidden');
+
+  one.classList.add('one');
+  two.classList.add('two');
+  three.classList.add('three');
 }
 
 export function spinnerOff() {
   refs.spinner.classList.add('visually-hidden');
+
+  one.classList.remove('one');
+  two.classList.remove('two');
+  three.classList.remove('three');
 }
